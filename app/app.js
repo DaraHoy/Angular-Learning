e1cb244f2474725a34614ea5b0d2a6a67d2fb087
Dara Hoy <darahoy@gmail.com>
2018-02-02 17:17:53 +0000

    Setup practice files

---
+++ app/app.js
@@ -0,0 +1,10 @@
+/*global angular*/
+(function(){
+    console.log("hi")
+    var app = angular.module('carRental', []);
+    
+    app.controller('RentalController', function(){
+        
+    });
+    
+})();
