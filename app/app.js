ec772ab7949a2e04e688ac56cd6d0023bb4df61b
Dara Hoy <darahoy@gmail.com>
2018-02-03 22:19:24 +0000

    Add rental objects

--- app/app.js
+++ app/app.js
@@ -1,10 +1,37 @@
 /*global angular*/
 (function(){
-    console.log("hi")
     var app = angular.module('carRental', []);
+    var cars = [
+            {
+                name: 'Toyota Prius',
+                type: "Compact",
+                description: ". . .",
+                price: 50
+            },
+            {
+                name: 'Honda Civic',
+                type: "Compact",
+                description: ". . .",
+                price: 50
+            }
+        ]
     
     app.controller('RentalController', function(){
-        
+        this.products = cars;
     });
+    
+    /* 
+        Car image
+        Car type
+        Car description
+        Daily rental price: add a filter to print as currency
+        Rent now button
+        Add something else of your own choosing.
+        If the car is not available
+        Car image
+        Car type
+        Unavailable shown
+    */
+    
     
 })();
