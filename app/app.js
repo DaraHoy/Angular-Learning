6d0b006231bf48f8bc9c9f3463733f0a392095f2
Dara Hoy <darahoy@gmail.com>
2018-02-07 17:56:41 +0000

    Add car unavailable features and rent controls

--- app/app.js
+++ app/app.js
@@ -1,104 +1,108 @@
 /*global $ angular*/
-(function(){
+(function() {
 
     var app = angular.module('carRental', []);
 
-    app.controller('RentalController', function(){
+    app.controller('RentalController', function($scope) {
         this.info = company;
         this.rentals = rentals;
+
+        this.rent = function(type, id) {
+            this.info.available--;
+            this.rentals[type][id].available--;
+        }
     });
 
     var company = {
         name: "Angular Rentals",
-        types: [{
-            type: "Compact",
-            available: 10,
-            price: 50
-        },{
-            type: "SUV",
-            available: 7,
-            price: 150
-        },{
-            type: "Luxury",
-            available: 5,
-            price: 550
-        }]
-
+        types: ["Compact", "SUV", "Lexury"],
+        available: 90
     }
 
-    var rentals = [[{
+    var rentals = [
+        [{
                 name: 'Toyota Prius',
                 description: "The Toyota Prius is a full hybrid electric automobile developed by Toyota",
                 price: 50,
-                img: "../img/prius.png"
+                img: "../img/prius.png",
+                available: 10,
+                type: 0,
+                id: 0
             },
             {
                 name: 'Ford Fiesta',
                 description: ". . .",
                 price: 50,
-                img: "../img/fiesta.png"
-            },{
+                img: "../img/fiesta.png",
+                available: 10,
+                type: 0,
+                id: 1
+            }, {
                 name: 'Ford Focus',
                 description: ". . .",
                 price: 50,
-                img: "../img/focus.png"
-            }],[{
-                name: 'Chevrolet Tahoe',
-                type: "SUV",
-                description: ". . .",
-                price: 150,
-                img: "../img/tahoe.png"
-            },{
-                name: 'GMC Yukon-Denali',
-                type: "SUV",
-                description: ". . .",
-                price: 150,
-                img: "../img/denali.png"
-            },{
-                name: 'Ford Explorer',
-                type: "SUV",
-                description: ". . .",
-                price: 150,
-                img: "../img/explorer.png"
-            }],[{
-                name: 'Jaguar XF',
-                type: "Premium",
-                description: ". . .",
-                price: 550,
-                img: "../img/jaguarXF.png"
-            },{
-                name: 'Chrysler 300',
-                type: "Premium",
-                description: ". . .",
-                price: 550,
-                img: "../img/300.png"
-            },{
-                name: 'Mercedes E350',
-                type: "Premium",
-                description: ". . .",
-                price: 550,
-                img: "../img/mercedes.png"
-            }]
-        ]
-
-    /*
-        Car image
-        Car type
-        Car description
-        Daily rental price: add a filter to print as currency
-        Rent now button
-        Add something else of your own choosing.
-        If the car is not available
-        Car image
-        Car type
-        Unavailable shown
-    */
-
+                img: "../img/focus.png",
+                available: 10,
+                type: 0,
+                id: 2
+            }
+        ],
+        [{
+            name: 'Chevrolet Tahoe',
+            type: "SUV",
+            description: ". . .",
+            price: 150,
+            img: "../img/tahoe.png",
+            available: 10,
+            type: 1,
+            id: 0
+        }, {
+            name: 'GMC Yukon-Denali',
+            type: "SUV",
+            description: ". . .",
+            price: 150,
+            img: "../img/denali.png",
+            available: 10,
+            type: 1,
+            id: 1
+        }, {
+            name: 'Ford Explorer',
+            type: "SUV",
+            description: ". . .",
+            price: 150,
+            img: "../img/explorer.png",
+            available: 10,
+            type: 1,
+            id: 2
+        }],
+        [{
+            name: 'Jaguar XF',
+            type: "Premium",
+            description: ". . .",
+            price: 550,
+            img: "../img/jaguar.png",
+            available: 10,
+            type: 2,
+            id: 0
+        }, {
+            name: 'Chrysler 300',
+            type: "Premium",
+            description: ". . .",
+            price: 550,
+            img: "../img/300.png",
+            available: 10,
+            type: 2,
+            id: 1
+        }, {
+            name: 'Mercedes E350',
+            type: "Premium",
+            description: ". . .",
+            price: 550,
+            img: "../img/mercedes.png",
+            available: 10,
+            type: 2,
+            id: 2
+        }]
+    ];
 
 })();
-
-//Materialize inits
-$(document).ready(function() {
-    $('select').material_select();
-    $('ul.tabs').tabs();
-});
